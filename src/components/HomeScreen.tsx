d133adfffed8fa3fb1045768e33ac8f4b6d5b5f3
[dyad] <starshine0011@gmail.com>
2025-10-07 23:29:25 +0500

    [dyad] Extracted animated background into a reusable component - wrote 2 file(s)

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -13,6 +13,7 @@ import {
   Crown
 } from 'lucide-react';
 import { Button } from './ui/button'; // Import the new Button component
+import AnimatedBackground from './AnimatedBackground'; // Import the new AnimatedBackground component
 
 interface HomeScreenProps {
   onPlay: () => void;
@@ -71,12 +72,7 @@ function HomeScreen({ onPlay, onStore, onDashboard, onSettings }: HomeScreenProp
   return (
     <div className="relative w-full h-full flex flex-col mx-auto">
       {/* Animated Background */}
-      <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute top-10 left-10 w-20 h-20 bg-blue-500/20 rounded-full animate-blob-1"></div>
-        <div className="absolute top-32 right-16 w-16 h-16 bg-purple-500/20 rounded-full animate-blob-2"></div>
-        <div className="absolute bottom-40 left-20 w-12 h-12 bg-pink-500/20 rounded-full animate-blob-3"></div>
-        <div className="absolute bottom-20 right-10 w-24 h-24 bg-green-500/20 rounded-full animate-blob-4"></div>
-      </div>
+      <AnimatedBackground />
 
       {/* Header */}
       <div className="relative z-10 p-6 text-center">
